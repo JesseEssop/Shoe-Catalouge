56b178b080873fc81357eb165e61ee26476de4ce
JesseEssop <jsteez16@gmail.com>
2019-08-05 15:28:46 +0200

    2nd Draft

--- Shoe-log.js
+++ Shoe-log.js
@@ -4,139 +4,139 @@ var shoes = [
         brand : "Nike",
         size : 9,
         price : 350,
-        in_stock : 5
+        stock : 5
     },
     {
         color : 'orange',
         brand : "Adidas",
         size : 4,
         price : 300,
-        in_stock : 3
+        stock : 3
     },
     {
         color : 'red',
         brand : "Jordan",
         size : 11,
         price : 500,
-        in_stock : 2
+        stock : 2
     },
     {
         color : 'yellow',
         brand : "Converse",
         size : 5,
         price : 275,
-        in_stock : 6
+        stock : 6
     },
     {
         color : 'white',
         brand : "Puma",
         size: 7,
         price : 400,
-        in_stock : 8
+        stock : 8
     },
     {
         color : 'white',
         brand : "Adidas",
         size : 8,
         price : 450,
-        in_stock : 10
+        stock : 10
     },
     {
         color : 'yellow',
         brand : "Nike",
         size: 8,
         price : 430,
-        in_stock : 6
+        stock : 6
     },
     {
         color : 'orange',
         brand : "Converse",
         size : 4,
         price : 300,
-        in_stock : 5
+        stock : 5
     },
     {
         color : 'red',
         brand : "Puma",
         size : 11,
         price : 410,
-        in_stock : 11
+        stock : 11
     },
     {
         color : 'yellow',
         brand : "Adidas",
         size : 10,
         price : 480,
-        in_stock : 8
+        stock : 8
     },
     {
         color : 'blue',
         brand : "Converse",
         size: 8,
         price : 380,
-        in_stock : 6
+        stock : 6
     },
     {
         color : 'white',
         brand : "Puma",
         size: 7,
         price : 420,
-        in_stock : 8
+        stock : 8
     },
     {
         color : 'orange',
         brand : "Nike",
         size : 6,
         price : 390,
-        in_stock : 3
+        stock : 3
     },
     {
         color : 'blue',
         brand : "Adidas",
         size : 5,
         price : 440,
-        in_stock : 7
+        stock : 7
     },
     {
         color : 'white',
         brand : "Converse",
         size : 8,
         price : 300,
-        in_stock : 10
+        stock : 10
     },
     {
         color : 'yellow',
         brand : "Puma",
         size: 9,
         price : 430,
-        in_stock : 7
+        stock : 7
     },
     {
         color : 'white',
         brand : "Nike",
         size: 9,
         price : 480,
-        in_stock : 9
+        stock : 9
     },
     {
         color : 'yellow',
         brand : "Jordan",
         size: 6,
         price : 520,
-        in_stock : 5
+        stock : 5
     },
     {
         color : 'blue',
         brand : "Jordan",
         size: 7,
         price : 500,
-        in_stock : 3
+        stock : 3
     },
     {
         color : 'white',
         brand : "Jordan",
-        size: 11,
+        size  :  11,
         price : 550,
-        in_stock : 5
+        stock : 5
     }
 ];
--- shoe-dom.js
+++ shoe-dom.js
@@ -1,14 +1,26 @@
-var addBtn = document.querySelector(".addBtn");
+var addBasketBtn = document.querySelector(".addBasketBtn");
 var searchBtn = document.querySelector(".searchBtn");
 var checkoutBtn = document.querySelector(".checkoutBtn");
 var removeBtn = document.querySelector(".removeBtn");
 var brandDropDown = document.querySelector(".shoeDropDown");
 var sizeDropDown = document.querySelector(".shoeSizeDropDown");
 var colourDropDown = document.querySelector(".shoeColourDropDown");
+
 var addShoeBtn = document.querySelector(".addShoeBtn");
 var update = document.querySelector(".update");
+var searchBtn = document.querySelector(".searchBtn");
+var addBtn = document.querySelector(".addBtn");
+var message = document.querySelector(".message");
 
-var shoeInsance = ShoeFactory();
+var brandInput = document.querySelector(".Brand");
+var sizeInput = document.querySelector(".Size");
+var colourInput = document.querySelector(".Colour");
+var priceInput = document.querySelector(".Price");
+var stockInput = document.querySelector(".Stock")
+
+
+
+var shoeInstance = ShoeFactory();
 var active;
 
 window.onload = function () {
@@ -22,6 +34,45 @@ addShoeBtn.addEventListener("click", function () {
         active = true;
     } else {
         update.style.display = 'none';
-        active = false;
+        acive = false;
     }
+});
+
+addBtn.addEventListener("click", function () {
+
+    var newBrand = brandInput.value;
+    var newColour = colourInput.value;
+    var newSize = sizeInput.value;
+    var newPrice = priceInput.value;
+    var newStock = stockInput.value;
+
+   message.innerHTML = shoeInstance.add(newBrand, newColour, newSize, newPrice, newStock);
+   
+   
+    if (newBrand === "" && newColour === "" && newSize === "" && newPrice === "" && newStock === "") {
+        return message.innerHTML = "Please fill in all fields";
+    
+    }
+
+
+
+
+});
+
+searchBtn.addEventListener("click", function (){
+    var findshoe = shoeInstance.select(brandDropDown.value, colourDropDown.value, sizeDropDown.value );
+
+    message.innerHTML = shoeInstance.text()
+    
+    if (brandDropDown.value === '' || sizeDropDown.value === '' || colourDropDown.value === '' ){
+        return message.innerHTML = "Please select all options to find shoe"    
+    }
+});
+
+addBasketBtn.addEventListener("click", function(){
+    shoeInstance.basket();
+    
+
+
+
 });
